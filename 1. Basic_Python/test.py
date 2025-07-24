736ec9d2fe79b51aaaa382939b4dd8f112df8524
Sitthikorn Mangkala <157336868+M-Sitthikorn@users.noreply.github.com>
2025-07-24 20:17:30 +0700

    numpy laboratory

---
+++ 1. Basic_Python/test.py
@@ -0,0 +1,12 @@
+# create dictionaries  key : value
+# SNAKE CASE     CAMEL CASE groupIdmTum
+group_idm = { 
+        1 : 'film',
+        2 : 'tam',
+        3 : 'kade',
+        4 : 'nice',
+        5 : 'mai',
+}
+group_idm[6] = 'tum'  # Add element to dictionaries
+# print(group_idm) 
+print(group_idm[4]) # get value if key is 4 
